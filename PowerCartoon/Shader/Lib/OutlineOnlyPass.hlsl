a5300eee74b95b89b0fdaf9aa0165a0516aa7dd9
ken <redcool007@qq.com>
2022-04-12 15:30:51 +0800

    update OutlineOnlyPass.hlsl

--- PowerCartoon/Shader/Lib/OutlineOnlyPass.hlsl
+++ PowerCartoon/Shader/Lib/OutlineOnlyPass.hlsl
@@ -5,8 +5,9 @@
 struct appdata
 {
     float4 vertex : POSITION;
-    float3 normal:NORMAL;
     float2 uv:TEXCOORD;
+    half3 normal:NORMAL;
+    half3 color:COLOR;
 };
 
 struct v2f
@@ -18,28 +19,28 @@ struct v2f
 sampler2D _OutlineTex;
 
 CBUFFER_START(UnityPerMaterial)
-float _Width;
-float4 _Color;
-float4 _OutlineTex_ST;
+half _Width;
+half4 _Color;
+half4 _OutlineTex_ST;
 CBUFFER_END
 
 v2f vert (appdata v)
 {
     v2f o;
     o.vertex = TransformObjectToHClip(v.vertex.xyz);
-    o.vertex.z *= 0.9;
-    float3 worldNormal = TransformObjectToWorldNormal(v.normal);
-    float3 normalClip = mul((float3x3)UNITY_MATRIX_VP,worldNormal);
-    o.vertex.xy += normalClip.xy * _Width * o.vertex.w;
+    // o.vertex.z *= 0.9;
+    half3 worldNormal = TransformObjectToWorldNormal(v.normal);
+    half3 normalClip = mul((half3x3)UNITY_MATRIX_VP,worldNormal);
+    o.vertex.xy += normalClip.xy * _Width * o.vertex.w * v.color.x;
 
     o.uv= TRANSFORM_TEX(v.uv,_OutlineTex);
     
     return o;
 }
 
-float4 frag (v2f i) : SV_Target
+half4 frag (v2f i) : SV_Target
 {
-    float4 col = tex2D(_OutlineTex,i.uv) * _Color;
+    half4 col = tex2D(_OutlineTex,i.uv) * _Color;
     return col;
 }
 #endif //OUTLINE_ONLY_PASS_HLSL
