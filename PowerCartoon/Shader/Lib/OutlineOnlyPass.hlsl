c289213996d1ba3d9e2ff16610ff5dc26983c2b0
ken <redcool007@qq.com>
2022-11-09 20:37:52 +0800

    v2.0.2.2 update OutlineOnly.shader add Noise vertex wave add noise uv scroll

--- PowerCartoon/Shader/Lib/OutlineOnlyPass.hlsl
+++ PowerCartoon/Shader/Lib/OutlineOnlyPass.hlsl
@@ -1,6 +1,7 @@
 #if !defined(OUTLINE_ONLY_PASS_HLSL)
 #define OUTLINE_ONLY_PASS_HLSL
 #include "../../../PowerShaderLib/Lib/UnityLib.hlsl"
+#include "../../../PowerShaderLib/Lib/NoiseLib.hlsl"
 
 struct appdata
 {
@@ -13,42 +14,85 @@ struct appdata
 struct v2f
 {
     float4 vertex : SV_POSITION;
-    float2 uv:TEXCOORD;
+    float4 uv:TEXCOORD;
     half4 color:COLOR;
 };
 
 sampler2D _OutlineTex;
+sampler2D _NoiseMap;
 
 CBUFFER_START(UnityPerMaterial)
-half _Width;
+half _Width,_WidthLocalYAtten;
 half4 _Color;
 half4 _OutlineTex_ST;
 half _VertexColorAttenOn;
-half _ShowPolyEdge;
+half _ZOffset;
+
+half _NoiseWaveScale;
+float _BaseLocalY;
+float _NoiseAlphaScale;
+
+float4 _NoiseMap_ST;
 CBUFFER_END
 
 #define MUL_VERTEX_COLOR_ATTEN(v) (_VertexColorAttenOn? v.color.x : 1)
 
 v2f vert (appdata v)
 {
+    float3 worldPos = mul(unity_ObjectToWorld,v.vertex);
+    float3 wn = UnityObjectToWorldNormal(v.normal);
+    float3 viewDir = UnityWorldSpaceViewDir(worldPos);
+    float rnv = 1 - saturate(dot(wn,viewDir));
+
+    float localYAtten = saturate(v.vertex.y - _BaseLocalY);
+    
+    #if defined(_NOISE_VERTEX_ON)
+        float noise = tex2Dlod(_NoiseMap,float4(v.uv.xy * _NoiseMap_ST.xy + _NoiseMap_ST.zw *_Time.xx,0,0));
+        noise *= _NoiseWaveScale;
+        noise *= localYAtten;
+        noise *= rnv;
+        v.vertex.xyz += v.normal * noise;
+    #endif
+
     v2f o;
     o.vertex = TransformObjectToHClip(v.vertex.xyz);
-    o.vertex.z *= _ShowPolyEdge ? 0.9 : 1;
+    o.vertex.z *= _ZOffset;
+    o.uv.z = rnv;
 
-    half3 normalView = mul((half3x3)UNITY_MATRIX_IT_MV,v.normal);
-    half3 normalClip = normalize(TransformViewToProjection(normalView));
+    float3 normalView = mul((float3x3)UNITY_MATRIX_IT_MV,v.normal);
+    float3 normalClip = normalize(TransformViewToProjection(normalView));
 
-    o.vertex.xy += normalClip.xy * _Width * o.vertex.w * MUL_VERTEX_COLOR_ATTEN(v)*0.1;
+    float2 outlineOffset = (normalClip.xy) * _Width * o.vertex.w * 0.1;
+    outlineOffset *= MUL_VERTEX_COLOR_ATTEN(v);
+    outlineOffset *= smoothstep(0.,0.3,saturate(v.vertex.y - _WidthLocalYAtten));
+    o.vertex.xy += outlineOffset;
 
     o.color = v.color;
-    o.uv= TRANSFORM_TEX(v.uv,_OutlineTex);
+    o.uv.xy = v.uv;
     
     return o;
 }
 
 half4 frag (v2f i) : SV_Target
 {
-    half4 col = tex2D(_OutlineTex,i.uv) * _Color * MUL_VERTEX_COLOR_ATTEN(i);
+    // float noise = GradientNoise(i.uv * _NoiseST.xy + _Time.xx* _NoiseST.zw) ;
+    float noise = 0;
+    #if defined(_NOISE_MAP_ON)
+    noise = tex2D(_NoiseMap,i.uv * _NoiseMap_ST.xy + _NoiseMap_ST.zw *_Time.xx);
+    #endif
+    // clip(noise);
+    
+    float2 uv = i.uv * _OutlineTex_ST.xy + _OutlineTex_ST.zw + noise;
+    half4 col = tex2D(_OutlineTex,uv) * _Color * MUL_VERTEX_COLOR_ATTEN(i);
+
+    #if defined(_NOISE_MAP_ON)
+    col.a *= saturate(noise*_NoiseAlphaScale);
+    #endif
+
+    #if defined(_APPLY_FRESNEL)
+    col.a *= i.uv.z;
+    #endif
+    
     return col;
 }
 #endif //OUTLINE_ONLY_PASS_HLSL
--- PowerCartoon/Shader/OutlineOnly.shader
+++ PowerCartoon/Shader/OutlineOnly.shader
@@ -2,19 +2,30 @@ Shader "Unlit/OutlineOnly"
 {
     Properties
     {
+        [Header(Outline)]
         _OutlineTex("_OutlineTex",2d) = "white"{}
         _Color("_Color",color)  =(1,1,1,1)
-        _Width("_Width",range(0.002,.1)) = 0.01
+        _Width("_Width",range(0.002,0.2)) = 0.01
+        _WidthLocalYAtten("_WidthLocalYAtten",float) = -1
+        _ZOffset("_ZOffset",range(0,1)) = 0.9
 
-        [Header(Atten)]
         [Toggle(_VERTEX_COLOR_ATTEN)]_VertexColorAttenOn("_VertexColorAttenOn",float) = 1
+        [Toggle(_APPLY_FRESNEL)]_ApplyFresnel("_ApplyFresnel",int) = 0
 
-        [Header(Blend)]
+        [Header(Noise)]
+        [Toggle(_NOISE_MAP_ON)]_NoiseMapOn("_NoiseMapOn",int) = 0
+        _NoiseMap("_NoiseMap",2d) = ""{}
+        _NoiseAlphaScale("_NoiseAlphaScale",float) = 1
+
+        [Header(Noise Vertex)]
+        [Toggle(_NOISE_VERTEX_ON)]_NoiseVertexOn("_NoiseVertexOn",int) = 0
+        _NoiseWaveScale("_NoiseWaveScale",float) = 0.3
+        _BaseLocalY("_BaseLocalY",float) = 0
+
+        [Header(Render States)]
         [Enum(UnityEngine.Rendering.BlendMode)]_SrcMode("_SrcMode",int) = 5
         [Enum(UnityEngine.Rendering.BlendMode)]_DstMode("_DstMode",int) = 10
-
-        [Header(Poly Edge)]
-        [GroupToggle]_ShowPolyEdge("_ShowPolyEdge",int) = 0
+        [Enum(UnityEngine.Rendering.CullMode)]_CullMode("_CullMode",int) = 1
     }
     SubShader
     {
@@ -24,7 +35,7 @@ Shader "Unlit/OutlineOnly"
 
         Pass
         {
-            cull front
+            cull [_CullMode]
             // ztest greater
             // zwrite off
 
@@ -32,6 +43,9 @@ Shader "Unlit/OutlineOnly"
             #pragma vertex vert
             #pragma fragment frag
             #pragma shader_feature_local _VERTEX_COLOR_ATTEN
+            #pragma shader_feature_local_vertex _NOISE_VERTEX_ON
+            #pragma shader_feature_local_fragment _NOISE_MAP_ON
+            #pragma shader_feature_local_fragment _APPLY_FRESNEL
 
             #include "Lib/OutlineOnlyPass.hlsl"
             ENDHLSL
